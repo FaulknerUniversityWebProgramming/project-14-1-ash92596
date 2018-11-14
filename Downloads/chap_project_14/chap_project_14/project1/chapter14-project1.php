08760bc418c1c84dc4c04ca4d8aefb74b8fda767
ash92596 <ashleigh.presley@faulkner.edu>
2018-11-14 15:28:25 -0600

    Updated Step 4

--- Downloads/chap_project_14/chap_project_14/project1/chapter14-project1.php
+++ Downloads/chap_project_14/chap_project_14/project1/chapter14-project1.php
@@ -1,9 +1,3 @@
-<?php
-
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,11 +12,8 @@
 
     <link rel="stylesheet" href="css/styles.css">
     
-    
     <script   src="https://code.jquery.com/jquery-1.7.2.min.js" ></script>
-       
     <script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
-    
 </head>
 
 <body>
@@ -35,9 +26,7 @@
     
     <main class="mdl-layout__content mdl-color--grey-50">
         <section class="page-content">
-
             <div class="mdl-grid">
-
               <!-- mdl-cell + mdl-card -->
               <div class="mdl-cell mdl-cell--3-col card-lesson mdl-card  mdl-shadow--2dp">
                 <div class="mdl-card__title mdl-color--orange">
@@ -67,23 +56,20 @@
                               echo "<ul style='list-style: none;'>";
                                 while($row = $result->fetch_assoc()) {
                                   echo "<li><a href='?employee=" . $row["EmployeeID"] ."'><p>" . $row["FirstName"] . " " . $row["LastName"] . "</p></a></li>";
-                                  //echo "<li><a href='#'><p>" . $row["FirstName"] . " " . $row["LastName"] . "</p></a></li>";
-                                }
+                                  }
                               echo "</ul>";
                             } else {
                                 echo "0 results";
                             }
-
-                            $conn->close();                      
+                      
+                             $conn->close();   
                          ?>            
-
                     </ul>
                 </div>
               </div>  <!-- / mdl-cell + mdl-card -->
               
               <!-- mdl-cell + mdl-card -->
               <div class="mdl-cell mdl-cell--9-col card-lesson mdl-card  mdl-shadow--2dp">
-
                     <div class="mdl-card__title mdl-color--deep-purple mdl-color-text--white">
                       <h2 class="mdl-card__title-text">Employee Details</h2>
                     </div>
@@ -93,62 +79,55 @@
                               <a href="#address-panel" class="mdl-tabs__tab is-active">Address</a>
                               <a href="#todo-panel" class="mdl-tabs__tab">To Do</a>
                           </div>
-                        
                           <div class="mdl-tabs__panel is-active" id="address-panel">
                               
-                           <?php
-                                          $link = mysqli_connect("localhost", "root", "ashleigh1", "book"); 
+                     <?php
+                           $link = mysqli_connect("localhost", "root", "ashleigh1", "book"); 
+                                 if($link === false){ 
+                                     die("ERROR: Could not connect. " 
+                                         . mysqli_connect_error()); 
+                                 } 
+                                  
+                                  $sql = "SELECT Employees.FirstName, Employees.LastName, Employees.Address, Employees.City, Employees.Region, Employees.Country, Employees.Postal, Employees.Email, Employees.EmployeeID
+                                          FROM Employees 
+                                          INNER JOIN EmployeeToDo ON Employees.EmployeeID=EmployeeToDo.EmployeeID WHERE Employees.EmployeeID=". $_GET['employee'] . " GROUP BY Employees.EmployeeID"; 
 
-                                          if($link === false){ 
-                                              die("ERROR: Could not connect. " 
-                                                          . mysqli_connect_error()); 
-                                          } 
-
-                                          $id = $_GET['employee'];
-                                          $sql = "SELECT FirstName, LastName, Address, City, Region, Country, Postal, Email, Employees.EmployeeID
-                                                  FROM Employees INNER JOIN EmployeeToDo ON Employees.EmployeeID=EmployeeToDo.EmployeeID WHERE Employees.EmployeeID=". $_GET['employee']; 
-                                          if($res = mysqli_query($link, $sql)){ 
-                                              if(mysqli_num_rows($res) > 0){ 
-                                                echo "<ul style='list-style: none;'>";
-                                                  while($row = mysqli_fetch_array($res)){ 
-                                                      echo "<li><p>" . $row["FirstName"] . 
-                                                        " " . $row["LastName"] . "<br>" . $row["Address"] . 
-                                                        "<br>" . $row["City"] . ", " . $row["Region"] . 
-                                                        "<br>" . $row["Country"] . ", " . $row["Postal"] . 
-                                                        "<br>" . $row["Email"] . "</p></li>";
-                                                  } 
-                                                  echo "</ul>";
-                                                  mysqli_free_result($res); 
-                                              } else{ 
+                                        if($res = mysqli_query($link, $sql)){ 
+                                            if(mysqli_num_rows($res) > 0){ 
+                                                while($row = mysqli_fetch_array($res)){ 
+                                                    echo "<p>" . $row["FirstName"] . 
+                                                      " " . $row["LastName"] . "<br>" . $row["Address"] . 
+                                                      "<br>" . $row["City"] . ", " . $row["Region"] . 
+                                                      "<br>" . $row["Country"] . ", " . $row["Postal"] . 
+                                                      "<br>" . $row["Email"] . "</p>";
+                                                 }
+                                              
+                                                mysqli_free_result($res); 
+                                             
+                                            } else{ 
                                                   echo "No Matching records are found."; 
-                                              } 
+                                             }
                                           } else{ 
                                               echo "ERROR: Could not able to execute $sql. "  
-                                                                          . mysqli_error($link); 
-                                          } 
-
+                                                    . mysqli_error($link); 
+                                            }
                                           mysqli_close($link); 
                                       ?> 
                            
          
                           </div>
-                          <div class="mdl-tabs__panel" id="todo-panel">
-                              
-                               <?php                       
-                                 /* retrieve for selected employee;
-                                    if none, display message to that effect */
-                               ?>  
+                          
+                          <div class="mdl-tabs__panel" id="todo-panel">  
                                   <tbody>
-                                   
+                                    
                                     <?php
                                           $link = mysqli_connect("localhost", "root", "ashleigh1", "book"); 
 
                                           if($link === false){ 
                                               die("ERROR: Could not connect. " 
-                                                          . mysqli_connect_error()); 
+                                                    . mysqli_connect_error()); 
                                           } 
 
-                                          $id = $_GET['employee'];
                                           $sql = "SELECT * FROM EmployeeToDo WHERE EmployeeID=". $_GET['employee']; 
                                           if($res = mysqli_query($link, $sql)){ 
                                               if(mysqli_num_rows($res) > 0){ 
@@ -171,29 +150,23 @@
                                                   mysqli_free_result($res); 
                                               } else{ 
                                                   echo "No Matching records are found."; 
-                                              } 
+                                                } 
                                           } else{ 
                                               echo "ERROR: Could not able to execute $sql. "  
-                                                                          . mysqli_error($link); 
-                                          } 
+                                                    . mysqli_error($link); 
+                                            } 
 
                                           mysqli_close($link); 
                                       ?> 
-
-                                </table>
-                           
-         
-                          </div>
-                        </div>                         
-                    </div>    
-  
-                 
-              </div>  <!-- / mdl-cell + mdl-card -->   
-            </div>  <!-- / mdl-grid -->    
-
-        </section>
+                                    
+                </div>
+              </div>                         
+            </div>
+          </div>  <!-- / mdl-cell + mdl-card -->   
+        </div>  <!-- / mdl-grid -->    
+      </section>
     </main>    
-</div>    <!-- / mdl-layout --> 
-          
+  </div>    <!-- / mdl-layout --> 
+  
 </body>
 </html>
